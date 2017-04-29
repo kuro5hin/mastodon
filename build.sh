9c87fcbde58394ffc586ec323f19db477cc4c860
kuro5hin <rusty@kuro5hin.org>
2017-04-29 15:00:25 -0400

    Make the build script take a tag arg, and also push

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-docker build --rm -t kuro5hin/mastodon:latest .
-
+TAG="latest"
+if [ $1 ]; then
+	TAG=$1
+fi
+echo "Building kuro5hin/mastodon:"$TAG
+docker build --rm -t kuro5hin/mastodon:$TAG .
+echo "---------------------------------------------------------------\nDone! Pushing...\n"
+docker push kuro5hin/mastodon:$TAG
